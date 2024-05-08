8536578f62484fc22e96155493353503ae50ad77
Coding WIth Ahmed <155929233+ahmedprogrammer2005@users.noreply.github.com>
2024-05-08 11:10:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,6 @@ function calculateAge() {
     var months = now.getMonth() - dob.getMonth();
     var days = now.getDate() - dob.getDate();
 
-    if (months < 0 || (months === 0 && days < 0)) {
-        years--;
-        months += 12;
-    }
-
-    if (days < 0) {
-        var prevMonthDays = new Date(now.getFullYear(), now.getMonth(), 0).getDate();
-        days += prevMonthDays;
-        months--;
-    }
-
     var ageString = years + " years, " + months + " months, " + days + " days";
     document.getElementById('age').innerText = ageString;
 }
